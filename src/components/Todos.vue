26bab19b4ecace394bdc3776b6fb50185451bacf
Hlaing Min Than(Faizal) <hlaingminthan92@gmail.com>
2021-09-17 21:25:15 +0630

    Vuex7-delete-todo

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -8,7 +8,12 @@
       <div class="row">
         <div class="col-md-4 my-4" v-for="todo in myTodos" :key="todo.id">
           <b-card bg-variant="primary" text-variant="white" class="text-center">
-            <b-card-text>{{ todo.title }}</b-card-text>
+            <b-card-text class="d-flex justify-content-between">
+              <span>{{ todo.title }}</span>
+              <span @click="deleteTodo(todo.id)"
+                ><b-icon icon="trash-fill" variant="danger"></b-icon
+              ></span>
+            </b-card-text>
           </b-card>
         </div>
       </div>
@@ -22,7 +27,7 @@ import AddTodo from "./AddTodo.vue";
 export default {
   components: { AddTodo },
   computed: mapGetters(["myTodos"]),
-  methods: mapActions(["getTodos"]),
+  methods: mapActions(["getTodos", "deleteTodo"]),
   mounted() {
     this.getTodos();
   },
--- src/store/modules/Todos.js
+++ src/store/modules/Todos.js
@@ -15,6 +15,11 @@ export default {
     setTodo(state, newTodo) {
       state.todos.unshift(newTodo);
     },
+    removeTodo(state, removeId) {
+      state.todos = state.todos.filter((todo) => {
+        return todo.id != removeId;
+      });
+    },
   },
   actions: {
     async getTodos({ commit }) {
@@ -28,6 +33,12 @@ export default {
         newTodo
       );
       commit("setTodo", res.data);
+    },
+    async deleteTodo(context, removeId) {
+      await axios.delete(
+        `https://jsonplaceholder.typicode.com/todos/${removeId}`
+      );
+      context.commit("removeTodo", removeId);
     },
   },
 };
